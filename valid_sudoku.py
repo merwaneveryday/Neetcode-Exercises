2b165fc3fdec0875ce939e37fb20620e09d163e2
Merwan <66710609+merwaneveryday@users.noreply.github.com>
2024-07-31 15:47:14 +0200

    Update valid_sudoku.py

--- valid_sudoku.py
+++ valid_sudoku.py
@@ -21,3 +21,41 @@ class Solution:
                 squares[(r // 3, c // 3)].add(board[r][c])
 
         return True
+
+
+board = [
+    ["1", "2", ".", ".", "3", ".", ".", ".", "."],
+    ["4", ".", ".", "5", ".", ".", ".", ".", "."],
+    [".", "9", "8", ".", ".", ".", ".", ".", "3"],
+    ["5", ".", ".", ".", "6", ".", ".", ".", "4"],
+    [".", ".", ".", "8", ".", "3", ".", ".", "5"],
+    ["7", ".", ".", ".", "2", ".", ".", ".", "6"],
+    [".", ".", ".", ".", ".", ".", "2", ".", "."],
+    [".", ".", ".", "4", "1", "9", ".", ".", "8"],
+    [".", ".", ".", ".", "8", ".", ".", "7", "9"]
+]
+
+def sudokuSolving(board):
+    cols = defaultdict(set)
+    rows = defaultdict(set)
+    squares = defaultdict(set)  # key = (r // 3, c // 3)
+
+    for r in range(9):
+        for c in range(9):
+            if board[r][c] == '.':
+                continue
+            if (
+                board[r][c] in rows[r]
+                or board[r][c] in cols[c]
+                or board[r][c] in squares[r // 3, c // 3]
+            ):
+                return False
+            
+            cols[c].add(board[r][c])
+            rows[r].add(board[r][c])
+            squares[r // 3, c // 3].add(board[r][c])
+
+    return True  # Return True only after checking all cells
+
+if __name__ == "__main__":
+    print(sudokuSolving(board))
